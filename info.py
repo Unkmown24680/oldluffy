9871ee2d57ea09629133ee1f6bb8f55b89caa2cd
Unkmown24680 <144600694+Unkmown24680@users.noreply.github.com>
2024-02-09 20:59:15 +0530

    Update info.py

--- info.py
+++ info.py
@@ -34,7 +34,7 @@ STREAM_API = (environ.get('STREAM_API', 'e02963f90c4578126dee38fcf6c89548e575991
 STREAMHTO = (environ.get('STREAMHTO', 'https://t.me/openlink_movierequest/5'))
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1720819569').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1720819569 6251724935').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001960613454').split()] #Channel id for auto indexing ( make sure bot is admin )
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
