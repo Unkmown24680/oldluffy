6cfc46158a5be9b82f5e13815aad723ca509976a
Unkmown24680 <144600694+Unkmown24680@users.noreply.github.com>
2024-02-27 22:30:17 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -371,6 +371,7 @@ async def start(client, message):
                 ]]
                 )
             )
+        
             await asyncio.sleep(600)
             await k.edit("<b>ʏᴏᴜʀ ᴍᴇꜱꜱᴀɢᴇ ɪꜱ ᴅᴇʟᴇᴛᴇᴅ !\nᴋɪɴᴅʟʏ ꜱᴇᴀʀᴄʜ ᴀɢᴀɪɴ.</b>")
             return
